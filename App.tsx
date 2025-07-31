2fb8663ba017762d5df41ca19d160cd6c5c21b54
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-07-31 11:40:18 -0300

    Learnc React Native fundamentals

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    color:'#fff',
+    backgroundColor: '#00000',
     alignItems: 'center',
     justifyContent: 'center',
   },
