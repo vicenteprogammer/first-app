c44ae773606666be1ad4e48dc0f05d270f58d74e
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-08-01 09:03:11 -0300

    Comments about learn

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    //Diferente do html, aqui no react native colocamso os tevxtos, imagens e outros elemntos visuais para a nossa aplicação através de tags reservadas. Como está no código abaixo, mas é muito similar as tags html.
     <View style={styles.container}>
       <Text>Hello World</Text>
       <StatusBar style="auto" />
@@ -10,6 +11,9 @@ export default function App() {
   );
 }
 
+//Para os styles da nossa aplicação não usamos mais o Css, agora vamos usar o StyleSheet, que é um objeto que podemos importar do react native que possibilitar criar os styles.
+
+//Algumas diferenças do Css estão na sintaxe, pois aqui no styleSheet vamos escrever com camelCase as propriedades, como se fosse uma vriavel dentro do objeto.
 const styles = StyleSheet.create({
   container: {
     flex: 1,
